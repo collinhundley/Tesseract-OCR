c1a1f805a1bf897d26d31dea7655125222d7a814
Thies C. Arntzen <thieso@gmail.com>
2020-08-04 22:07:53 +0200

    add missing curl

--- TesseractOCR/macos/update-deps.sh
+++ TesseractOCR/macos/update-deps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-FILES="leptonica-1.80.0 giflib-5.2.1 jpeg-9d libpng-1.6.37 libtiff-4.1.0 openjpeg-2.3.1 little-cms2-2.11 webp-1.1.0 tesseract-4.1.1"
+FILES="leptonica-1.80.0 giflib-5.2.1 jpeg-9d libpng-1.6.37 libtiff-4.1.0 openjpeg-2.3.1 little-cms2-2.11 webp-1.1.0 tesseract-4.1.1 curl-7.71.1"
 OS="mojave"
 
 rm -rf lib include
